e9a1f3a66051c71b63be0ef1e0f0418faf26c69e
Pimeine <112776350+Pimeine@users.noreply.github.com>
2022-11-25 18:24:06 +0100

    AreaNameDisplay Update
    • Added the scrit to display area name 5s after the player laoded a scene.

--- GameOff2022_RPG/Assets/Level/DONOTTOUCH_TestArea/Niels/AreaName.cs
+++ GameOff2022_RPG/Assets/Level/DONOTTOUCH_TestArea/Niels/AreaName.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// PLACE THIS SCRIPT INSIDE A LEVEL TO DISPLAY IT'S NAME OR SOMETHING ON SCREEN FOR 5s WHEN THE PLAYER LOAD IT.
+
 public class AreaName : MonoBehaviour
 {
     // Start is called before the first frame update
